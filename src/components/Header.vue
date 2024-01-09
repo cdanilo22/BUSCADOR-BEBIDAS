4919495c58e17c468a24dd33be82eb93d2f33854
CesarDan <cdeleonc4@miumg.edu.gt>
2024-01-09 11:53:44 -0600

    Se añadieron dos rutas e imagen en el header de inicio

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,49 @@
 <script setup>
 
+import {computed} from 'vue'
+import { RouterLink, useRoute } from 'vue-router';
+const route = useRoute()
+const paginaInicio = computed(() => route.name === 'inicio')
+
 </script>
 
 <template>
-    <header>
+    <header 
+    class="bg-slate-800"
+    :class="{header: paginaInicio}"
+    >
         <div class="mx-auto container px-5 py-16">
-            <div>
+            <div class="flex justify-between items-center">
+                <div>
+                    <RouterLink
+                    :to="{name: 'inicio'}"
+                    >
+                     <img class="w-32" src="/img/logo.svg" alt="Logotipo">
+                    </RouterLink>
+                </div>
 
+                <nav class="flex gap-4">
+                    <RouterLink
+                    :to="{name: 'inicio'}"
+                    class="text-white uppercase font-bold"
+                    active-class="text-orange-500"
+                    >
+                        Inicio
+                    </RouterLink>
+
+                    <RouterLink
+                    :to="{name: 'favoritos'}"
+                    class="text-white uppercase font-bold"
+                    active-class="text-orange-500"
+                    >
+                        Favoritos
+                    </RouterLink>
+                </nav>
             </div>
-
-    
-
         <form
          class="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6"
-        >
+         v-if="paginaInicio"
+         >
 
         <div class="space-y-4">
              <label 
@@ -43,10 +73,18 @@
         class="bg-orange-800 hover:bg-orange-900 cursor-pointer text-white font-extrabold w-full p-2 rounded-lg uppercase"
         value="Buscar Recetas"
         >
-
         </form>
     </div>
     </header>
 </template>
+
+<style>
+    .header{
+        background-image: url('/public/img/bg.jpg');
+        background-size: cover;
+        background-position: center;
+    }
+
+</style>
 
 
