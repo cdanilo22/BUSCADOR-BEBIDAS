a1506a3691d93657960352442497e3a9e647a692
CesarDan <cdeleonc4@miumg.edu.gt>
2024-01-12 15:03:12 -0600

    Agregando recetas a favoritos y almacenando en localStorae

--- src/stores/favoritos.js
+++ src/stores/favoritos.js
@@ -1,15 +1,29 @@
-import {ref} from 'vue'
+import {ref, watch} from 'vue'
+import {useBebidasStore} from './bebidas'
 import {defineStore} from 'pinia'
 
 export const useFavoritosStore = defineStore('favoritos', () => {
 
+    const bebidas = useBebidasStore()
+    const favoritos = ref([])
+
+    watch(favoritos, () => {
+        sincronizarLocalStorage()
+    }, {
+        deep: true
+    })
+
+    const sincronizarLocalStorage = () => {
+        localStorage.setItem('favoritos', JSON.stringify(favoritos.value))
+    }
+
     const handleClickFavorito = () => {
-        console.log('agregando...');
+        favoritos.value.push(bebidas.receta)
     }
 
 
     return {
+        favoritos,
         handleClickFavorito
-    }
-
+    } 
 })
